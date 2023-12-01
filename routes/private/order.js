7f6f66396af2486dc9143866f85d7f5212bdda3b
faozan099 <125945029+faozan099@users.noreply.github.com>
2023-12-01 16:19:55 +0700

    update

--- routes/private/order.js
+++ routes/private/order.js
@@ -3,7 +3,7 @@ const authenticateToken = require('../../middleware/Auth')
 
 const router = require('express').Router()
 
-router.post('/product/order', addOrder)
+router.post('/product/order',[authenticateToken], addOrder)
 router.get('/poruct/order/:user_buyer_id',[authenticateToken], getOrderUser)
 
 module.exports = router
